3efe5d319dc99f291e5160613cdaa323ef3ea08d
wajsbrot <nkthiebaut@gmail.com>
2015-10-13 17:13:36 +0200

    Improved conv_net with objective function

--- conv_net.py
+++ conv_net.py
@@ -13,32 +13,43 @@ from lasagne.layers import InputLayer
 from lasagne.layers import DropoutLayer
 from lasagne.layers import Conv2DLayer
 from lasagne.layers import MaxPool2DLayer
-
+from lasagne.nonlinearities import softmax
 from lasagne.layers import get_all_params
+from lasagne.updates import nesterov_momentum
 
 from nolearn.lasagne import NeuralNet
-from lasagne.nonlinearities import softmax
+from nolearn.lasagne import BatchIterator
+from nolearn.lasagne import TrainSplit
+
 from utils import make_submission_file
 
+import numpy as np
+
 X, y, images_id = cPickle.load(open('train.pkl', 'rb'))
+#X = X[:-1]
+#y = y.reshape(-1,1).astype(np.float32)
+
+print "Train:"
+print "X.shape:", X.shape
+print "y.shape:", y.shape
 
 layers0 = [
     # layer dealing with the input data
     (InputLayer, {'shape': (None, X.shape[1], X.shape[2], X.shape[3])}),
 
     # first stage of our convolutional layers
-    (Conv2DLayer, {'num_filters': 96, 'filter_size': 5}),
+#    (Conv2DLayer, {'num_filters': 96, 'filter_size': 5}),
 #    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
 #    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
 #    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
 #    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
-    (MaxPool2DLayer, {'pool_size': 2}),
+#    (MaxPool2DLayer, {'pool_size': 2}),
 
     # second stage of our convolutional layers
 #    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
 #    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
-    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
-    (MaxPool2DLayer, {'pool_size': 2}),
+#    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
+#    (MaxPool2DLayer, {'pool_size': 2}),
 
     # two dense layers with dropout
 #    (DenseLayer, {'num_units': 64}),
@@ -51,19 +62,22 @@ layers0 = [
 
 nouri_net = NeuralNet(
     layers0,
+
+    update=nesterov_momentum,
     update_learning_rate=0.01,
     update_momentum=0.9,
 
-    regression=True,
+    #batch_iterator_train=BatchIterator(batch_size=64),
+    #batch_iterator_test=BatchIterator(batch_size=62),
+
+    objective=regularization_objective,
+    objective_lambda2=0.0025,
+
+    train_split=TrainSplit(eval_size=0.25),
+    #regression=True,
     max_epochs=1000,
-    verbose=1,
+    verbose=3,
     )
-
-
-
-print "Train:"
-print "X.shape:", X.shape
-print "y.shape:", y.shape
 
 from nolearn.lasagne import PrintLayerInfo
 nouri_net.verbose = 3
@@ -80,5 +94,7 @@ with open('nouri_net.pkl', 'wb') as f:
 X_test, _, images_id = cPickle.load(open('test.pkl', 'rb'))
 
 predictions = nouri_net.predict_proba(X_test)
+
+print predictions
 
 make_submission_file(predictions, images_id)
--- utils.py
+++ utils.py
@@ -11,6 +11,8 @@ from datetime import date
 import pandas as pd
 
 from PIL import Image
+from nolearn.lasagne import objective
+
 
 def show_img(img_id, root='data/images', test=False):
     dir = 'test' if test else 'train'
@@ -38,3 +40,18 @@ def make_submission_file(predictions ,images_id, output_filepath="submission_"+s
     predictions_df = pd.DataFrame(predictions, index=images_id, columns=['genus'])
     predictions_df.index.names = ['id']
     predictions_df.to_csv(output_filepath)
+
+
+def regularization_objective(layers, lambda1=0., lambda2=0., *args, **kwargs):
+    # default loss
+    losses = objective(layers, *args, **kwargs)
+    # get the layers' weights, but only those that should be regularized
+    # (i.e. not the biases)
+    weights = get_all_params(layers[-1], regularizable=True)
+    # sum of absolute weights for L1
+    sum_abs_weights = sum([abs(w).sum() for w in weights])
+    # sum of squared weights for L2
+    sum_squared_weights = sum([(w ** 2).sum() for w in weights])
+    # add weights to regular loss
+    losses += lambda1 * sum_abs_weights + lambda2 * sum_squared_weights
+    return losses
