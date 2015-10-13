eb427ea5c4bd856d0cf5988f35992353627755dc
Ubuntu <ubuntu@ip-172-31-5-91.us-west-1.compute.internal>
2015-10-13 01:34:06 +0000

    Debugging layers shapes ...

--- conv_net.py
+++ conv_net.py
@@ -9,68 +9,59 @@ import sys
 sys.setrecursionlimit(10000)
 
 from lasagne import layers
+import numpy as np
+
+
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne.layers import DropoutLayer
+from lasagne.layers import Conv2DLayer
+from lasagne.layers import MaxPool2DLayer
+
 from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import BatchIterator
+from nolearn.lasagne import TrainSplit
 from lasagne.nonlinearities import softmax
 from utils import make_submission_file
 
-layers0 = [
-    # layer dealing with the input data
-    (InputLayer, {'shape': (None, X.shape[1], X.shape[2], X.shape[3])}),
+layers4 = [
+    (InputLayer, {'shape': (64,3, 200, 200)}),
 
-    # first stage of our convolutional layers
-    (Conv2DLayer, {'num_filters': 96, 'filter_size': 5}),
-    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
-    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
-    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
-    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
-    (MaxPool2DLayer, {'pool_size': 2}),
+    (Conv2DLayer, {'num_filters': 32, 'filter_size': (3, 3), 'pad': 1}),
+    (Conv2DLayer, {'num_filters': 32, 'filter_size': (3, 3), 'pad': 1}),
+    (Conv2DLayer, {'num_filters': 32, 'filter_size': (3, 3), 'pad': 1}),
+    (MaxPool2DLayer, {'pool_size': (2, 2)}),
 
-    # second stage of our convolutional layers
-    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
-    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
-    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
-    (MaxPool2DLayer, {'pool_size': 2}),
+    (Conv2DLayer, {'num_filters': 64, 'filter_size': (3, 3), 'pad': 1}),
+    (MaxPool2DLayer, {'pool_size': (2, 2)}),
 
-    # two dense layers with dropout
     (DenseLayer, {'num_units': 64}),
     (DropoutLayer, {}),
     (DenseLayer, {'num_units': 64}),
 
-    # the output layer
-    (DenseLayer, {'num_units': 10, 'nonlinearity': softmax}),
+    (DenseLayer, {'num_units': 1, 'nonlinearity': softmax}),
 ]
-
 nouri_net = NeuralNet(
-    layers=[
-        ('input', layers.InputLayer),
-        ('conv1', layers.Conv2DLayer),
-        ('pool1', layers.MaxPool2DLayer),
-        ('conv2', layers.Conv2DLayer),
-        ('pool2', layers.MaxPool2DLayer),
-        ('conv3', layers.Conv2DLayer),
-        ('pool3', layers.MaxPool2DLayer),
-        ('hidden4', layers.DenseLayer),
-        ('hidden5', layers.DenseLayer),
-        ('output', layers.DenseLayer),
-        ],
-    input_shape=(None, 1, 96, 96),
-    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
-    conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
-    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
-    hidden4_num_units=500, hidden5_num_units=500,
-    output_num_units=30, output_nonlinearity=softmax,
-
+    layers4,
     update_learning_rate=0.01,
     update_momentum=0.9,
+    batch_iterator_train=BatchIterator(batch_size=64),
+	train_split=TrainSplit(eval_size=0.25),
 
-    regression=True,
-    max_epochs=1000,
+    max_epochs=10,
     verbose=1,
     )
-
-
+#
+#from nolearn.lasagne import PrintLayerInfo
+#nouri_net.verbose = 3
+#nouri_net.initialize()
+#layer_info = PrintLayerInfo()
+#layer_info(nouri_net)
+#
 X, y, images_id = cPickle.load(open('train.pkl', 'rb'))
-
+X=X[:-1]
+#y=y.reshape(-1,1)[:-1]
+y=y[:-1].astype(np.int32)
 print "Train:"
 print "X.shape:", X.shape
 print "y.shape:", y.shape
