22eb5db381a36eef4152d03445e5437785141f64
wajsbrot <nkthiebaut@gmail.com>
2015-11-07 20:05:45 +0100

    Minor debug.

--- preprocess.py
+++ preprocess.py
@@ -23,7 +23,7 @@ def prepare_data(name='', width = 224, balanced=True):
         DM.pad(224)
     DM.save_to_lasagne_format(filename='test'+name+'.npz')
 
-if __name__ == __main__:
+if __name__ == '__main__':
     prepare_data(name='_width_224_balanced', width=224, balanced=True)
     prepare_data(name='_width_224_unbalanced', width=224, balanced=False)
     prepare_data(name='_width_200_balanced', width=200, balanced=True)
