4d5d57daf4ea2a4f0a622b595499b74e5cf8b72d
wajsbrot <nkthiebaut@gmail.com>
2015-11-17 09:55:57 +0100

    Working on model averaging

--- average_models.py
+++ average_models.py
@@ -5,7 +5,8 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
-
+from datetime import date
+from utils import make_submission_file
 __author__ = 'thiebaut'
 __date__ = '16/11/15'
 
@@ -17,25 +18,39 @@ def average_models(model_file_a, model_file_b):
     df = df.drop(['genus_x', 'genus_y'], axis=1)
     return df
 
-df = pd.read_csv('models/training_VGG11_2015-11-13.csv')
+df = pd.read_csv('models/training_VGG11-maxout_2015-11-17.csv')
 y = pd.read_csv('data/train_labels.csv')
 df = pd.merge(df, y, on=['id'])
 y_pred = np.array(df['genus_x'], dtype=np.float64)
 y_true = np.array(df['genus_y'], dtype=np.float64)
 print "Model A AUC-ROC: ", roc_auc_score(y_true, y_pred)
 
+"""
 df = pd.read_csv('models/training_VGG11-maxout_2015-11-15.csv')
 y = pd.read_csv('data/train_labels.csv')
 df = pd.merge(df, y, on=['id'])
 y_pred = np.array(df['genus_x'], dtype=np.float64)
 y_true = np.array(df['genus_y'], dtype=np.float64)
 print "Model B AUC-ROC: ", roc_auc_score(y_true, y_pred)
-
-average = average_models('models/training_VGG11_2015-11-13.csv', 'models/training_VGG11-maxout_2015-11-15.csv')
+"""
+average = average_models('models/training_VGG11_2015-11-13.csv', 'models/training_VGG11-maxout_2015-11-17.csv')
 y_avg = np.array(average['genus'], dtype=np.float64)
 print "Average models AUC-ROC: ", roc_auc_score(y_true, y_avg)
 
+test_labels = 'SubmissionFormat.csv'
+y_test = pd.read_csv("data/" + test_labels, index_col=0)
+images_id_test = np.array(y_test.index.tolist())
+name = '_' + str(date.today())
 
+def make_submission_file(predictions, images_id, output_filepath="submission_" + str(date.today()) + ".csv"):
+    predictions_df = pd.DataFrame(predictions, index=images_id, columns=['genus'])
+    predictions_df.index.names = ['id']
+    predictions_df.to_csv(output_filepath)
+
+predictions = average_models('submissions/submission_VGG11_2015-11-13.csv',
+                             'submissions/submission_VGG11-maxout_2015-11-17.csv')['genus'].values
+
+make_submission_file(predictions, images_id_test, output_filepath='submissions/avg_submission'+name+'.csv')
 """
 y_bis = []
 for pred in y_pred:
