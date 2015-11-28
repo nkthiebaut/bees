5727bfce86902772edf4b97a3cbafad11a751281
wajsbrot <nkthiebaut@gmail.com>
2015-11-28 13:17:24 +0100

    Just renamed a few files.

--- average_models.py
+++ average_models.py
@@ -11,9 +11,11 @@ __author__ = 'thiebaut'
 __date__ = '16/11/15'
 
 
-models = ['VGG11_2015-11-23', 'AlexNet_2015-11-22', 'VGG13-maxout_2015-11-23',
-          'VGG11-maxout_2015-11-23', 'VGG13_2015-11-23',
-          'reformed-gamblers_2015-11-23']
+models = ['VGG11_2015-11-23', 'VGG11-maxout_2015-11-23',
+          'VGG13_2015-11-23', 'VGG13-maxout_2015-11-23',
+          'AlexNet_2015-11-22',#, 'reformed-gamblers_2015-11-23',
+          'VGG11-maxout_2015-11-28', 'VGG13-maxout_2015-11-28',
+          'MyNet_2015-11-27'] #, 'AlexNet_2015-11-27']
 
 model_files = ['models/training_'+m+'.csv' for m in models]
 test_files = ['submissions/submission_'+m+'.csv' for m in models]
@@ -47,7 +49,7 @@ for m in model_files:
 
 dfs = map(pd.read_csv, model_files)
 predictions = reduce(average_models, dfs)
-outfile= 'train.csv'
+outfile = 'train.csv'
 predictions.to_csv(outfile, index=False)
 compute_auc_roc_and_accuracy(predictions)
 
